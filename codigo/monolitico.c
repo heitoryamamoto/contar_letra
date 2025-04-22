0b46cee8c085b8e3fe8b05626c910ee23901b258
heitoryamamoto <132718821+heitoryamamoto@users.noreply.github.com>
2025-04-22 07:47:48 -0300

    Update monolitico.c

--- codigo/monolitico.c
+++ codigo/monolitico.c
@@ -5,18 +5,25 @@ int main() {
     char str[100];
     int i = 0, count = 0;
 
+R1:
     printf("Digite uma string: ");
     fgets(str, sizeof(str), stdin);
     str[strcspn(str, "\n")] = '\0';
+    goto R2;
 
-inicio:
-    if (str[i] == '\0') goto fim;
+R2:
+    if (str[i] == '\0') goto R4;
+    goto R3;
 
+R3:
     count++;
     i++;
-    goto inicio;
+    goto R2;
 
-fim:
+R4:
     printf("\nResultado final: A string tem %d caracteres.\n", count);
+    goto R5;
+
+R5:
     return 0;
 }
